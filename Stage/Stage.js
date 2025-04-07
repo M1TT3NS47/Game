8c4ea6fa9dc9a39297c3081dc5fb86cc0965f46a
TheDrawingTotems <mitvin1947@students.uplifteducation.org>
2025-04-07 10:40:54 -0500

    Update Stage.js

--- Stage/Stage.js
+++ Stage/Stage.js
@@ -1,1 +1,22 @@
 
+import {
+  Stage as StageBase,
+  Trigger,
+  Watcher,
+  Costume,
+  Color,
+  Sound,
+} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+export default class Stage extends StageBase {
+  constructor(...args) {
+    super(...args);
+
+    this.costumes = [
+      new Costume("backdrop1", "./Stage/Costume/backround.jpeg", {
+        x: 0,
+        y: 0,
+      }),
+    ];
+  }
+}
